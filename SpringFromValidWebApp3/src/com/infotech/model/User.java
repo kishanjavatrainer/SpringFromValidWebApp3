c9422610275e00db750ab2d520929c5fa2219fcf
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-06-21 17:55:41 +0530

    SpringFromValidWebApp3 source code

---
+++ SpringFromValidWebApp3/src/com/infotech/model/User.java
@@ -0,0 +1,64 @@
+package com.infotech.model;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class User {
+
+	@NotEmpty
+	@Pattern(regexp="[^0-9]+")
+	@Size(min=6,max=20)
+	private String name;
+	
+	@Min(value = 12)
+	@Max(value = 120)
+	@NotNull
+	private Integer age;
+	
+	@NotEmpty
+	@Email
+	private String email;
+	
+	@NotEmpty
+	private String country;
+	
+	public User(String name, Integer age, String email, String country) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.email = email;
+		this.country = country;
+	}
+	public User() {
+	}
+	public String getName() {
+		return name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+}
